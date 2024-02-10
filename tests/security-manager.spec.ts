863b0a3325ff1ce8801d66b6d2c4a337a8813f84
Guilherme-07062002 <guilherme762002@gmail.com>
2024-02-10 13:23:32 -0300

    test: add mock implementation for passwords dont match case

--- tests/security-manager.spec.ts
+++ tests/security-manager.spec.ts
@@ -1,14 +1,33 @@
 import SecurityManager from "../src/security-manager";
 
+const passwordsDontMatchCase = () => {
+  return jest.fn()
+    .mockImplementationOnce(() => 'Guilherme')
+    .mockImplementationOnce(() => 'Guilherme Gomes')
+    .mockImplementationOnce(() => '12345679')
+    .mockImplementationOnce(() => '12345678');
+}
+
+const SuccessCase = () => {
+  return jest.fn()
+    .mockImplementationOnce(() => 'Guilherme')
+    .mockImplementationOnce(() => 'Guilherme Gomes')
+    .mockImplementationOnce(() => '12345678')
+    .mockImplementationOnce(() => '12345678');
+}
+
 jest.mock('prompt-sync', () => {
+  // Counter to control how many times the mock function is called
+  let callCount = 0;
   return jest.fn().mockImplementation(() => {
-    return jest.fn()
-      .mockImplementationOnce(() => 'Guilherme')
-      .mockImplementationOnce(() => 'Guilherme Gomes')
-      .mockImplementationOnce(() => '12345678')
-      .mockImplementationOnce(() => '12345678')
-  })
-})
+    jest.clearAllMocks();
+    callCount++;
+    if (callCount === 2) {
+      return passwordsDontMatchCase();
+    }
+    return SuccessCase();
+  });
+});
 
 describe('testing mock security manager', () => {
   it('should create a new user', () => {
@@ -20,5 +39,16 @@ describe('testing mock security manager', () => {
     expect(consoleSpy).toHaveBeenNthCalledWith(3, 'Enter your password')
     expect(consoleSpy).toHaveBeenNthCalledWith(4, 'Re-enter your password')
     expect(consoleSpy).toHaveBeenNthCalledWith(5, `Saving Details for User (Guilherme, Guilherme Gomes, 87654321)\n`)
+  })
+
+  it('should alert if passwords do not match', () => {
+    const consoleSpy = jest.spyOn(console, 'log')
+    SecurityManager.createUser()
+    expect(consoleSpy).toBeCalledTimes(5)
+    expect(consoleSpy).toHaveBeenNthCalledWith(1, 'Enter a username')
+    expect(consoleSpy).toHaveBeenNthCalledWith(2, 'Enter your full name')
+    expect(consoleSpy).toHaveBeenNthCalledWith(3, 'Enter your password')
+    expect(consoleSpy).toHaveBeenNthCalledWith(4, 'Re-enter your password')
+    expect(consoleSpy).toHaveBeenNthCalledWith(5, `The passwords don't match`)
   })
 })
